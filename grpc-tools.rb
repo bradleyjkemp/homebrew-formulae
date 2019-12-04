8a220695d27688d9e9d252d37ed950f620d2e621
goreleaserbot <goreleaser@carlosbecker.com>
2019-12-04 22:19:10 +0000

    Brew formula update for grpc-tools version v0.2.4

--- grpc-tools.rb
+++ grpc-tools.rb
@@ -2,16 +2,16 @@
 class GrpcTools < Formula
   desc "A suite of gRPC debugging tools. Like Fiddler/Charles but for gRPC."
   homepage "https://github.com/bradleyjkemp/grpc-tools"
-  version "0.2.3"
+  version "0.2.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/bradleyjkemp/grpc-tools/releases/download/v0.2.3/grpc-tools_0.2.3_macOS_amd64.zip"
-    sha256 "72f482feedf93051d1084c4b39667d77120aae2db12cbd9d2c8a42c336d9d5e0"
+    url "https://github.com/bradleyjkemp/grpc-tools/releases/download/v0.2.4/grpc-tools_0.2.4_macOS_amd64.zip"
+    sha256 "14d5c3c81fe5dc5453f4be0e7a5d9fb5aef7fcd2e4c22fb4de363fe3b9a310c2"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/bradleyjkemp/grpc-tools/releases/download/v0.2.3/grpc-tools_0.2.3_Linux_amd64.zip"
-      sha256 "f94482239b61a2f3ab5e30f393e72ae09821cd04f01ef600bd92b607312fc94b"
+      url "https://github.com/bradleyjkemp/grpc-tools/releases/download/v0.2.4/grpc-tools_0.2.4_Linux_amd64.zip"
+      sha256 "7ec77e1ff81f1eaa7b378dcce6b472665a33cdcdcac23d5a9da3a8bfd184d59c"
     end
   end
 
@@ -19,6 +19,5 @@ class GrpcTools < Formula
     bin.install "grpc-dump"
     bin.install "grpc-fixture"
     bin.install "grpc-replay"
-    bin.install "grpc-view"
   end
 end
