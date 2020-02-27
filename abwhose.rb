f59b174f26b694c08c11454ef74d8df519d0d312
goreleaserbot <goreleaser@carlosbecker.com>
2020-02-27 21:34:57 +0000

    Brew formula update for abwhose version v0.1.2

--- abwhose.rb
+++ abwhose.rb
@@ -7,11 +7,11 @@ class Abwhose < Formula
 
   if OS.mac?
     url "https://github.com/bradleyjkemp/abwhose/releases/download/v0.1.2/abwhose_0.1.2_macOS_amd64.zip"
-    sha256 "ec85940a77d73e0820a54d16d286c96e094b5d159f04b06b6fd8cda50460b4be"
+    sha256 "f8439c52c2b6a5dfe35aab755a62d13d5b23c3b0bf5e5ca88199389f3f5ea231"
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/bradleyjkemp/abwhose/releases/download/v0.1.2/abwhose_0.1.2_Linux_amd64.zip"
-      sha256 "209dc7e938e4e5146e3d5c4063f43a703872e9ed8ff37591d8e27cebde28c6ad"
+      sha256 "44c6a2c76d21d40ac9e03fd6d5938fe5488f4dacb94f3fe5b11fdde37c3b402d"
     end
   end
 
