08a64c6cda45dcc5b7e6befb0e25ff67b8a2f64e
goreleaserbot <goreleaser@carlosbecker.com>
2020-03-04 18:34:35 +0000

    Brew formula update for abwhose version v0.1.3

--- abwhose.rb
+++ abwhose.rb
@@ -2,16 +2,16 @@
 class Abwhose < Formula
   desc "The simplest way to find how to report abusive domains."
   homepage "https://github.com/bradleyjkemp/abwhose"
-  version "0.1.2"
+  version "0.1.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/bradleyjkemp/abwhose/releases/download/v0.1.2/abwhose_0.1.2_macOS_amd64.zip"
-    sha256 "f8439c52c2b6a5dfe35aab755a62d13d5b23c3b0bf5e5ca88199389f3f5ea231"
+    url "https://github.com/bradleyjkemp/abwhose/releases/download/v0.1.3/abwhose_0.1.3_macOS_amd64.zip"
+    sha256 "4b3bfd16c3637334adf346096172724f458b516654c3cf736c044f2e9cb44e53"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/bradleyjkemp/abwhose/releases/download/v0.1.2/abwhose_0.1.2_Linux_amd64.zip"
-      sha256 "44c6a2c76d21d40ac9e03fd6d5938fe5488f4dacb94f3fe5b11fdde37c3b402d"
+      url "https://github.com/bradleyjkemp/abwhose/releases/download/v0.1.3/abwhose_0.1.3_Linux_amd64.zip"
+      sha256 "32193e8bae8cb952532cb16a76c48cf66ed594bddfc6c15ddce39da6601aa99d"
     end
   end
 
