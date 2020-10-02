6b60e830eb450287634a54b81bf6492e37ec545e
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-02 18:51:32 +0100

    Brew formula update for sigma-test version v0.0.2

--- sigma-test.rb
+++ sigma-test.rb
@@ -2,16 +2,16 @@
 class SigmaTest < Formula
   desc "A test case runner for Sigma rules."
   homepage "https://github.com/bradleyjkemp/sigma-test"
-  version "0.0.1"
+  version "0.0.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/bradleyjkemp/sigma-test/releases/download/v0.0.1/sigma-test_0.0.1_macOS_amd64.zip"
-    sha256 "c30d6b9d49760e4560a95408e5c974bdba00c37623bfb7461542bc13749433ec"
+    url "https://github.com/bradleyjkemp/sigma-test/releases/download/v0.0.2/sigma-test_0.0.2_macOS_amd64.zip"
+    sha256 "70c4a2961ab7f174f589793beb06b26eb4e1f3abab80623192a09d81d3994a1a"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/bradleyjkemp/sigma-test/releases/download/v0.0.1/sigma-test_0.0.1_Linux_amd64.zip"
-      sha256 "77b7353635420fef5a69c4209f4e886edede7f6b59b97a75a2309662ae0be98d"
+      url "https://github.com/bradleyjkemp/sigma-test/releases/download/v0.0.2/sigma-test_0.0.2_Linux_amd64.zip"
+      sha256 "79ab95de90e333529238b2a3cc72cc78a2b4c4be936febb445f7541b90ef4ce5"
     end
   end
 
