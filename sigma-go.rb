3e30d20ab47477cb50ab55d6344f7e31db68c829
goreleaserbot <goreleaser@carlosbecker.com>
2022-03-25 11:16:26 +0000

    Brew formula update for sigma-go version v0.3.3

--- sigma-go.rb
+++ sigma-go.rb
@@ -5,23 +5,31 @@
 class SigmaGo < Formula
   desc "A Go implementation and parser of Sigma rules."
   homepage "https://github.com/bradleyjkemp/sigma-go"
-  version "0.3.2"
+  version "0.3.3"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/bradleyjkemp/sigma-go/releases/download/v0.3.2/sigma-go_0.3.2_macOS_amd64.zip"
-      sha256 "0b8e5a7d9bc7f7292c08fd253a1c127402753d2b9aef46a64c9932b81db4b42b"
+    url "https://github.com/bradleyjkemp/sigma-go/releases/download/v0.3.3/sigma-go_0.3.3_macOS_amd64.zip"
+    sha256 "f55d1e1a9fe35b8224dbd61890deebd7adbba649549f7859e3557868507ca430"
 
-      def install
-        bin.install "sigmac"
+    def install
+      bin.install "sigmac"
+    end
+
+    if Hardware::CPU.arm?
+      def caveats
+        <<~EOS
+          The darwin_arm64 architecture is not supported for the SigmaGo
+          formula at this time. The darwin_amd64 binary may work in compatibility
+          mode, but it might not be fully supported.
+        EOS
       end
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/bradleyjkemp/sigma-go/releases/download/v0.3.2/sigma-go_0.3.2_Linux_amd64.zip"
-      sha256 "79559c53221741dee8bab02e2187084e9120b1bdd397e3280528563fe444743b"
+      url "https://github.com/bradleyjkemp/sigma-go/releases/download/v0.3.3/sigma-go_0.3.3_Linux_amd64.zip"
+      sha256 "a6a4a812eefa6066eaa1464a527525e6f0dea221c7a209bde0d65942aa3a4c5f"
 
       def install
         bin.install "sigmac"
